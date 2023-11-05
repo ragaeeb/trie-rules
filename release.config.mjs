0e6710d78e2f494728ece939c11bc4d124c98e3c
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2023-11-06 00:13:53 +0400

    fix(release): NPM errors during publish

--- release.config.mjs
+++ release.config.mjs
@@ -5,5 +5,6 @@ export default {
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
         '@semantic-release/github',
+        '@semantic-release/npm',
     ],
 };
